cb15c55719024f609ffc0dd5440706bf324fbb41
Akbar Aziz <akbaraziz@gmail.com>
2021-07-09 22:22:26 -0500

    Updated Scripts

--- Windows - Eject CD-Rom.ps1
+++ Windows - Eject CD-Rom.ps1
@@ -1,4 +1,4 @@
 $sh = New-Object -ComObject "Shell.Application"
 $sh.Namespace(17).Items() | 
 Where-Object { $_.Type -eq "CD Drive" } | 
-foreach { $_.InvokeVerb("Eject") }
+ForEach-Object { $_.InvokeVerb("Eject") }
