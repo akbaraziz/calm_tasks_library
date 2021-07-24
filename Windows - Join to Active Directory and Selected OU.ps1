e64e69a9cd3b8f398a994cf8bbce38a148b631e5
Akbar Aziz <akbaraziz@gmail.com>
2021-07-24 13:55:44 -0500

    Updated domain variables

--- Windows - Join to Active Directory and Selected OU.ps1
+++ Windows - Join to Active Directory and Selected OU.ps1
@@ -2,8 +2,8 @@ $dc = "@@{DOMAIN_NAME}@@" # Specify the domain to join.
 $pw = "password" | ConvertTo-SecureString -asPlainText –Force # Specify the password for the domain admin.
 $usr = "$dc\administrator" # Specify the domain admin account.
 $creds = New-Object System.Management.Automation.PSCredential($usr, $pw)
-$ou = "OU=Servers,DC=contoso,DC=com" # Specify the OU to add the system.
+$ou = "OU=Servers,DC=lab,DC=local" # Specify the OU to add the system.
 $NewComputerName = "@@{HOST_NAME}@@" # Specify new Computer Name
 Add-Computer -DomainName $dc -Credential $creds -NewName $NewComputerName -OUPATH $ou -Force -Verbose
-Add-LocalGroupMember -Group "Administrators" -Member "Contoso\Administrator"
+Add-LocalGroupMember -Group "Administrators" -Member "Lab\Administrator"
 Restart-Computer -Force
