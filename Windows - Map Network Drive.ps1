d284ec5ea046b5791bca3d94896a668b0614d43b
Akbar Aziz <akbaraziz@gmail.com>
2021-07-24 19:36:36 -0500

    Initial Commit

--- Windows - Map Network Drive.ps1
+++ Windows - Map Network Drive.ps1
@@ -1,1 +1,4 @@
-New-PSDrive -Name "Z" -PSProvider FileSystem -Root "\\ntnx-files-1.lab.local\windows-apps" -Persist
+$User = "domain\username"
+$PWord = ConvertTo-SecureString -String "passgoeshere" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+New-PSDrive -Name "K" -PSProvider "FileSystem" -Root \\UNCPathGoesHere -Credential $Credential -Persist
